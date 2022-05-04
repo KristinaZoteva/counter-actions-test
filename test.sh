81881c4715ac01727a7c55c93ba76919e6b1c145
KristinaZoteva <kristina1994misha@gmail.com>
2022-05-04 16:03:19 +0300

    Feat: add issues action.

--- test.sh
+++ test.sh
@@ -6,14 +6,16 @@ on:
       - reopened
       - opened
 
+env:
+    TITLE: ${github.event.issue.title//\[\w+\]/}
+    PREPARED_TITLE: ${TITLE// /}
+    LENGTH_TITLE: ${#PREPARED_TITLE}
+
 jobs:
   comment-issues:
-    env:
-      TITLE: ${github.event.issue.title//\[\w+\]/}
-      PREPARED_TITLE: ${TITLE// /}
-      LENGTH_TITLE: ${#PREPARED_TITLE}
-    uses: KristinaZoteva/counter-actions-test/.github/workflows/check_issues.yaml@main
     if: '(${LENGTH_TITLE} < 4) || (#github.event.issue.body < 10)'
+    uses: KristinaZoteva/counter-actions-test/.github/workflows/check_issues.yaml@main
     with:
       REPO_PATH: 'https://github.com/KristinaZoteva/counter-actions-test/issues/new'
+    secrets:
       SECRET_ISSUES: ${{ secrets.SECRET_ISSUE }}
