91b188a6ff976dede5579d5b802e5d825b27eda8
KristinaZoteva <kristina1994misha@gmail.com>
2022-05-06 13:23:48 +0300

    Feat: add issues action.

--- test.sh
+++ test.sh
@@ -1,21 +1,7 @@
-name: "Issue comment"
+#!/bin/bash
 
-on:
-  issues:
-    types:
-      - reopened
-      - opened
-
-env:
-    TITLE: ${github.event.issue.title//\[\w+\]/}
-    PREPARED_TITLE: ${TITLE// /}
-    LENGTH_TITLE: ${#PREPARED_TITLE}
-
-jobs:
-  comment-issues:
-    if: '(${LENGTH_TITLE} < 4) || (#github.event.issue.body < 10)'
-    uses: KristinaZoteva/counter-actions-test/.github/workflows/check_issues.yaml@main
-    with:
-      REPO_PATH: 'https://github.com/KristinaZoteva/counter-actions-test/issues/new'
-    secrets:
-      SECRET_ISSUES: ${{ secrets.SECRET_ISSUE }}
+NAME='[Bug] Привет от меня [bug]'
+Title='jj'
+echo Title=$NAME | sed 's/ //g; s/\[bug\]//; s/\[Bug\]//; s/\[BUG\]//'
+TITLE_LENGTH=${#Title}
+echo $TITLE_LENGTH
